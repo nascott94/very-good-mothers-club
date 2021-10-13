9b1a33016b6b776f9d961d17591f03107b0e6c69
Dan Possehl <danielbpossehl@gmail.com>
2021-10-13 12:43:54 -0600

    switched to react bootstap

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,29 +1,22 @@
 import React from "react";
 import "./Navbar.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 function Navbar() {
   return (
     <div>
-      {" "}
-      <nav className="navbar bg-light">
-        <div className="container-fluid">
-          <a className="navbar">
-            <img
-              src="https://www.logo-designer.co/wp-content/uploads/2019/12/2019-stock-market-company-ned-davis-research-new-logo-design-4.png"
-              alt=""
-              width="30"
-              height="24"
-              class="d-inline-block align-text-top"
-            />
-            Bootstrap
-          </a>
-          <p>Login</p>
-          <p>Sign Up</p>
-          <p>Download APP</p>
-        </div>
-      </nav>
+      <Nav defaultActiveKey="/home" as="ul">
+        <Nav.Item as="li">
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
-
 export default Navbar;
