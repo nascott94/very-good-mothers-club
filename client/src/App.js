5753649e57d18d9668c0b6ff8fdcb0ab0f749ae9
Dan Possehl <danielbpossehl@gmail.com>
2021-10-13 11:34:29 -0600

    added app.js lineup for main

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   state = {
@@ -23,9 +24,9 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>HI</h1>
-      </div>
+      <main>
+        <Navbar />
+      </main>
     );
   }
 }
