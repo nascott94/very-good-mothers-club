b97771e40e6e677f56cba623239980f71588d5eb
Dan Possehl <danielbpossehl@gmail.com>
2021-10-18 11:15:48 -0600

    created scheema and model

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,24 @@
 import mongoose from "mongoose";
+
+const userSchema = mongoose.Scheema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+
+  passWord: String,
+  passWordver: String,
+  phoneNumber: Number,
+});
+
+const user = mongoose.model("user", userSchema);
+
+export default user;
