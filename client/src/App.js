e3d83daa6ec4ed2e338e1f6b928216caa52909dd
nascott <nascott94@outlook.com>
2021-10-20 11:24:22 -0600

    set up router and routes on app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
-import SignUp from "../src/components/SignUp/SignUp.js";
-import Profile from "../src/components/Profile/Profile.js";
 import Navbar from "../src/components/Navbar/Navbar.js";
+import Home from "../src/pages/Home.js";
+import SignUp from "../src/pages/SignUp.js";
+import Login from "../src/pages/Login.js";
+import Profile from "../src/pages/Profile.js";
+import Footer from "../src/components/Footer/Footer.js";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <main>
-        <Navbar />
-        <SignUp />
-        <Profile />
-      </main>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Navbar />
+          <div className="container">
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
