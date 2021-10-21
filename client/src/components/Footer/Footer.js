67e169c4ea8892ce397ded240b2a2092a532ea84
Dan Possehl <danielbpossehl@gmail.com>
2021-10-21 10:43:33 -0600

    working on navbar positioning

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import FolderIcon from "@mui/icons-material/Folder";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import "./Footer.css";
+
+export default function Footer() {
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const styles = {
+    jazz: {
+      // padding: 10,
+      // bottom: 10,
+      // top: 25,
+      position: "sticky",
+    },
+  };
+
+  return (
+    <BottomNavigation style={styles.jazz} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Recents"
+        value="recents"
+        icon={<RestoreIcon />}
+      />
+      <BottomNavigationAction
+        label="Favorites"
+        value="favorites"
+        icon={<FavoriteIcon />}
+      />
+      <BottomNavigationAction
+        label="Nearby"
+        value="nearby"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction
+        label="Folder"
+        value="folder"
+        icon={<FolderIcon />}
+      />
+    </BottomNavigation>
+  );
+}
